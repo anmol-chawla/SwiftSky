b12e35f65d8023eaa5d595273b5fb0d946628415
Anmol Chawla <anmolchawla10@gmail.com>
2024-11-16 19:36:12 +0530

    feat: implement integer

---
+++ SwiftSky/Packages/SwiftAtProtoLexicon/Sources/SwiftAtProtoLexicon/Types/LexInteger.swift
@@ -0,0 +1,25 @@
+//
+//  LexInteger.swift
+//  SwiftAtProtoLexicon
+//
+//
+
+struct LexInteger: BaseLexiconType, Codable {
+    var type: LexiconType { .INTEGER }
+    let description: String?
+    let `default`: [Int]
+    let minimum: Int?
+    let maximum: Int?
+    let `enum`: [Int]?
+    let const: Int?
+    
+    private enum TypedCodingKeys: String, CodingKey {
+        case type
+        case description
+        case `default`
+        case minimum
+        case maximum
+        case `enum`
+        case const
+    }
+}
