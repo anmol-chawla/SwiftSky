e703829be7b9546560ae362886e52ac379372783
Anmol Chawla <anmolchawla10@gmail.com>
2024-11-18 23:15:12 +0530

    chore: add cid link type

---
+++ SwiftSky/Packages/SwiftAtProtoLexicon/Sources/SwiftAtProtoLexicon/Types/LexCidLink.swift
@@ -0,0 +1,15 @@
+//
+//  LexCidLink.swift
+//  SwiftAtProtoLexicon
+//
+//
+
+struct LexCidLink: BaseLexiconType, Codable {
+    var type: LexiconType { .CID_LINK }
+    let description: String?
+    
+    private enum TypedCodingKeys: String, CodingKey {
+        case type
+        case description
+    }
+}
