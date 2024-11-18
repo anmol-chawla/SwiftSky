07ac9a9d3a63915bbdf2ccd2a13ee23f0764fd71
Anmol Chawla <anmolchawla10@gmail.com>
2024-11-18 23:14:57 +0530

    chore: add bytes type

---
+++ SwiftSky/Packages/SwiftAtProtoLexicon/Sources/SwiftAtProtoLexicon/Types/LexBytes.swift
@@ -0,0 +1,20 @@
+//
+//  LexBytes.swift
+//  SwiftAtProtoLexicon
+//
+//
+
+struct LexBytes: BaseLexiconType, Codable {
+    var type: LexiconType { .BYTES }
+    let description: String?
+    let maxLength: Int?
+    let minLength: Int?
+    
+    private enum TypedCodingKeys: String, CodingKey {
+        case type
+        case description
+        case maxLength
+        case minLength
+    }
+}
+
